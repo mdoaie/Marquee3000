6a07db112e7e60d06d9e0e69636425041a0fbc7d
Ezekiel Aquino <ezekiel@bakkenbaeck.no>
2017-05-23 20:20:44 +0200

    New page

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
-console.log('SEED3000');
+const FontFaceObserver = require('fontfaceobserver');
+const fontLeague = new FontFaceObserver('LeagueGothic');
+const fontRoboto = new FontFaceObserver('RobotoBlackItalic');
+const Marquee3k = require('marquee3000');
+
+Promise.all([fontRoboto.load()]).then(() => {
+  Marquee3k.init();
+});
